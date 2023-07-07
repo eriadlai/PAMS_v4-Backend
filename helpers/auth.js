4f8121e5b3d2e40b2b8db512d97644e7996d2426
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-07-07 14:00:29 -0700

    Update
    Base de tokens

---
+++ helpers/auth.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
+
+// ** NOTE **
+// res.locals.user has properties id and type as in the jwt itself.
+
+const adminAuth = (req, res, next) => {
+  const authHeader = req.header("authorization");
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    res.status(401).send();
+    return;
+  }
+
+  jwt.verify(token, TOKEN_SECRET, (err, data) => {
+    if (!data) {
+      res.status(401).send();
+      return;
+    }
+
+    const isAdmin = data.type === "ADMINISTRADOR" ? true : false;
+
+    if (err || !isAdmin) {
+      res.status(403).send();
+    } else {
+      res.locals.user = data;
+      next();
+    }
+  });
+};
+
+module.exports = {
+  adminAuth,
+};
